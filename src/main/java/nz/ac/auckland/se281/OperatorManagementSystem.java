cd44381bc316d96e8ae7a596ae3b062fbbfdf242
Kevkekiller49 <tlen226@aucklanduni.ac.nz>
2025-03-30 17:03:55 +1300

    Added Comments to code to help understand code better for someone else

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -10,15 +10,18 @@ public class OperatorManagementSystem {
   public OperatorManagementSystem() {}
 
   public void searchOperators(String keyword) {
+    // Removes empty space and changes keyword to all lowercase.
     String trimmedKeyword = keyword.trim().toLowerCase();
     int matchingOperatorsCount = 0;
 
+    // Loops through each of operator in the array list and applies condition.
     for (String operator : listOfOperators) {
       if (operator.toLowerCase().contains(trimmedKeyword) || trimmedKeyword.contains("*")) {
         matchingOperatorsCount++;
       }
     }
 
+    // Checks to make sure we have an operator we want and applies if statements.
     if (matchingOperatorsCount > 0) {
       if (matchingOperatorsCount == 1) {
         MessageCli.OPERATORS_FOUND.printMessage("is", "1", "", ":");
@@ -27,6 +30,7 @@ public class OperatorManagementSystem {
             "are", String.valueOf(matchingOperatorsCount), "s", ":");
       }
 
+      // Prints operator we want with * at the front.
       for (String operator : listOfOperators) {
         if (operator.toLowerCase().contains(trimmedKeyword) || trimmedKeyword.contains("*")) {
           System.out.println("* " + operator);
@@ -39,13 +43,17 @@ public class OperatorManagementSystem {
 
   public void createOperator(String operatorName, String location) {
 
+    // Using Types Location enum to format the location we get to what we want.
     Types.Location checkLocation = Types.Location.fromString(location);
     String fullName = checkLocation.getFullName();
     String city = checkLocation.getNameEnglish();
 
+    // Splits the operatorName at each space to turn it into an individual element.
     String[] words = operatorName.split(" ");
     String locationAbbreviation = checkLocation.getLocationAbbreviation();
 
+    // Empty string that ends up being the capitalized words of operatorName.
+    // Only capitalizes first letter of each word.
     String operatorNameWithCapitalizedWords = "";
     for (String word : words) {
       if (!word.isEmpty()) {
@@ -54,8 +62,11 @@ public class OperatorManagementSystem {
       }
     }
  
+    // Trims empty space incase nothing it added on at the end.
     operatorName = operatorNameWithCapitalizedWords.trim();
 
+    // Empty String
+    // Capitalize each character for the operatorAbbreviation.
     String operatorAbbreviation = "";
     for (String word : words) {
       char operatorTransform = Character.toUpperCase(word.charAt(0));
@@ -63,22 +74,27 @@ public class OperatorManagementSystem {
     }
 
     int number = 1;
-
+    // Checks if the Arraylist has a certain locationAbbreviation in it and adds number
+    // to keep track of operators.
     for (String operator : listOfOperators) {
       if (operator.contains("-" + locationAbbreviation + "-")) {
         number++;
       }
     }
 
+    // Error prevention if number of operators exceed 999.
     if (number >= 999) {
       System.out.println("Operator limit has been reached for " + city);
       return;
     }
 
+    // Formats string to have 3 digits in it (00x).
     String numberCount = String.format("%03d", number);
+    // Gets full name of operator that we want printed.
     String operatorFullName =
         (operatorAbbreviation + "-" + locationAbbreviation + "-" + numberCount);
 
+    // Checks if operator already exist.
     for (String operator : listOfOperators) {
 
       if (operator.contains(operatorName) && operator.contains("located in '" + fullName + "'")) {
@@ -88,9 +104,11 @@ public class OperatorManagementSystem {
       }
     }
 
+    // Adds operator to array list.
     listOfOperators.add(
         operatorName + " ('" + operatorFullName + "' located in '" + fullName + "')");
 
+    // Prints message we want using MessageCli.
     MessageCli.OPERATOR_CREATED.printMessage(operatorName, operatorFullName, fullName);
   }
 
